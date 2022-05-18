b29d9b9a0cfc33d5a1e73655189f6d3829ec0807
Alan Ruiz <alanzphy@gmail.com>
2022-05-17 21:17:45 -0600

    Add files via upload

---
+++ Ejerciciosdefunciones./Funcionconsuma.java
@@ -0,0 +1,13 @@
+public class Funcionconsuma {
+  public static void main(String[] args) {
+
+      int resultado;
+      resultado = suma(1, 2, 3);
+      System.out.println(resultado);
+  }
+
+  public static int suma(int a, int b, int c) {
+    return a + b + c;
+  }
+
+}
