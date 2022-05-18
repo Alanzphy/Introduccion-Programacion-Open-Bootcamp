d48b2a06a848911f46d843910fba5ab0a362ff35
Alan Ruiz <alanzphy@gmail.com>
2022-05-17 21:21:27 -0600

    Create doWhile.java

---
+++ Sentenciasdecontrol/doWhile.java
@@ -0,0 +1,15 @@
+package Sentenciasdecontrol;
+public class doWhile {
+
+  public static void main(String[] args) {
+    int numberodoWhile = 0;
+
+    do {
+      numberodoWhile++;
+      System.out.println(numberodoWhile);
+
+    } while (numberodoWhile < 0); {
+   
+    }
+  }
+}
